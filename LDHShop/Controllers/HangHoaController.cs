72a0e0c913f24db47cd01dbf2e59197b48eaa9ae
Hii <Hii@DESKTOP-HF8HOQU>
2024-04-12 14:23:21 +0700

    xong video 4

--- LDHShop/Controllers/HangHoaController.cs
+++ LDHShop/Controllers/HangHoaController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LDHShop.Data;
+using LDHShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LDHShop.Controllers
 {
     public class HangHoaController : Controller
     {
+        private readonly LdhshopContext db;
+
+        public HangHoaController(LdhshopContext context) {
+            db = context;
+        }
         public IActionResult Index(int? loai)
         {
-            return View();
+            var hanghoas = db.HangHoas.AsQueryable();
+
+            if(loai.HasValue)
+            {
+                hanghoas = hanghoas.Where(p => p.MaLoai == loai.Value);
+            }
+
+            var result = hanghoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTa = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+        public IActionResult Search(string query)
+        {
+            var hanghoas = db.HangHoas.AsQueryable();
+
+            if (query != null)
+            {
+                hanghoas = hanghoas.Where(p => p.TenHh.Contains(query));
+            }
+
+            var result = hanghoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTa = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
         }
     }
 }
---
+++ LDHShop/ViewModels/HangHoaVM.cs
@@ -0,0 +1,12 @@
+﻿namespace LDHShop.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string MoTa { get; set; }
+        public string TenLoai { get; set; }
+    }
+}
